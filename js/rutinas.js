4fed7cee5120ba122571312f9d57dfc46bef2800
Raj <Raj>
2016-05-22 03:56:25 -0400

    interactive game

---
+++ js/rutinas.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+$(document).ready(function() {
+    
+	$(".btnSig").click(function(e) {
+        var size = $('.slider').find('.elements').size();
+		$('.slider').find('.elements').each(
+			function(index,value){
+				if($(value).hasClass('e_visible')){
+					
+					$(value).hide();
+					$(value).removeClass('e_visible');
+					
+					if(index+1<size){
+						
+						$($('.slider').find('.elements').get(index+1)).slideDown();
+						$($('.slider').find('.elements').get(index+1)).addClass('e_visible');
+						return false;
+						
+					}else{
+						
+						$($('.slider').find('.elements').get(0)).slideDown();
+						$($('.slider').find('.elements').get(0)).addClass('e_visible');	
+						return false;
+						
+					}
+				}
+		});
+    });
+	
+});
